a6eb9601f14fc599b91b745a2ee3f0eca7dd5190
dam1b <dmarinoalonso@danielcastelao.org>
2021-02-12 09:10:47 +0100

    Initial commit

---
+++ src/MisMetodos.java
@@ -0,0 +1,13 @@
+/**
+ * Contiene métodos útiles para usar en los proyectos
+ */
+public class MisMetodos {
+    /**
+     * Método que monta un saludo
+     * @param mensaje saludo que quiero mostrar
+     * @return retorna con el mensaje y concatenado con otro String
+     */
+    public static String saludo(String mensaje){
+        return ("Saludo: " + mensaje);
+    }
+}
